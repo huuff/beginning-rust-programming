4899324fec6d818824d771fa32bd1d88b9f14993
Haf <haf@protonmail.ch>
2022-12-17 21:18:50 +0100

    sample file, and back to size 75 since 100 wouldn't fit my terminal

--- ch-2/src/main.rs
+++ ch-2/src/main.rs
@@ -6,11 +6,11 @@ use std::io::{BufRead, BufReader};
 use termion::clear;
 use termion::color;
 
-fn census(_world: [[u8; 100]; 100]) -> u16 {
+fn census(_world: [[u8; 75]; 75]) -> u16 {
     let mut count = 0;
 
-    for i in 0..99 {
-        for j in 0..99 {
+    for i in 0..74 {
+        for j in 0..74 {
             if _world[i][j] == 1 {
                 count += 1;
             }
@@ -19,11 +19,11 @@ fn census(_world: [[u8; 100]; 100]) -> u16 {
     count
 }
 
-fn generation(_world: [[u8; 100]; 100]) -> [[u8; 100]; 100] {
-    let mut new_world = [[0u8; 100]; 100];
+fn generation(_world: [[u8; 75]; 75]) -> [[u8; 75]; 75] {
+    let mut new_world = [[0u8; 75]; 75];
 
-    for i in 0..99 {
-        for j in 0..99 {
+    for i in 0..74 {
+        for j in 0..74 {
             let mut count = 0;
             if i > 0 {
                 count += _world[i-1][j];
@@ -31,19 +31,19 @@ fn generation(_world: [[u8; 100]; 100]) -> [[u8; 100]; 100] {
             if i > 0 && j > 0 {
                 count += _world[i-1][j+1];
             }
-            if i > 0 && j < 99 {
+            if i > 0 && j < 74 {
                 count += _world[i-1][j+1];
             }
-            if i < 99 {
+            if i < 74 {
                 count += _world[i+1][j];
             }
-            if i < 99 && j < 99 {
+            if i < 74 && j < 74 {
                 count += _world[i+1][j+1];
             }
             if j > 0 {
                 count += _world[i][j-1];
             }
-            if j < 99 {
+            if j < 74 {
                 count += _world[i][j+1];
             }
 
@@ -64,14 +64,14 @@ fn generation(_world: [[u8; 100]; 100]) -> [[u8; 100]; 100] {
 }
 
 fn main() {
-    let mut world = [[0u8; 100]; 100];
+    let mut world = [[0u8; 75]; 75];
     let mut generations = 0;
 
     let args : Vec<String> = env::args().collect();
 
     if args.len() < 2 {
-        for i in 0..99 {
-            for j in 0..99 {
+        for i in 0..74 {
+            for j in 0..74 {
                 if rand::random() {
                     world[i][j] = 1;
                 } else {
@@ -87,7 +87,9 @@ fn main() {
 
     println!("Population at generation {} is {}", generations, census(world));
 
-    for _gens in 0..100 {
+    let max_generations = env::args().nth(2).unwrap_or(String::from("100")).parse::<u16>().unwrap();
+
+    for _gens in 0..max_generations {
         let temp = generation(world);
         world = temp;
         generations += 1;
@@ -104,8 +106,8 @@ fn main() {
 
 }
 
-fn populate_from_file(filename: String) -> [[u8; 100]; 100] {
-    let mut new_world = [[0u8; 100]; 100];
+fn populate_from_file(filename: String) -> [[u8; 75]; 75] {
+    let mut new_world = [[0u8; 75]; 75];
     let file = File::open(filename).unwrap();
     let reader = BufReader::new(file);
     let mut pairs: Vec<(usize, usize)> = Vec::new();
@@ -118,14 +120,14 @@ fn populate_from_file(filename: String) -> [[u8; 100]; 100] {
         pairs.push((left.parse::<usize>().unwrap(), right.parse::<usize>().unwrap()));
     }
 
-    for i in 0..99 {
-        for j in 0..99 {
+    for i in 0..74 {
+        for j in 0..74 {
             new_world[i][j] = 0;
         }
     }
 
     for (x, y) in pairs {
-        if x > 99 || y > 99 {
+        if x > 74 || y > 74 {
             panic!("The provided pair {x}, {y} exceeds the grid bounds");
         }
         new_world[x][y] = 1;
@@ -133,9 +135,9 @@ fn populate_from_file(filename: String) -> [[u8; 100]; 100] {
     new_world
 }
 
-fn display_world(world: [[u8; 100]; 100]) {
-    for i in 0..99 {
-        for j in 0..99 {
+fn display_world(world: [[u8; 75]; 75]) {
+    for i in 0..74 {
+        for j in 0..74 {
             if world[i][j] == 1 {
                 print!("{red}*", red = color::Fg(color::Red));
             } else {
