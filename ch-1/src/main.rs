0f18b2b8d30f914fde4f24a96ea46ede4d8e046f
Haf <haf@protonmail.ch>
2022-12-17 19:52:04 +0100

    chapter 1, ex 1

--- ch-1/src/main.rs
+++ ch-1/src/main.rs
@@ -1,11 +1,11 @@
 extern crate rand;
 use std::{thread, time};
 
-fn census(_world: [[u8; 75]; 75]) -> u16 {
+fn census(_world: [[u8; 100]; 100]) -> u16 {
     let mut count = 0;
 
-    for i in 0..74 {
-        for j in 0..74 {
+    for i in 0..99 {
+        for j in 0..99 {
             if _world[i][j] == 1 {
                 count += 1;
             }
@@ -14,11 +14,11 @@ fn census(_world: [[u8; 75]; 75]) -> u16 {
     count
 }
 
-fn generation(_world: [[u8; 75]; 75]) -> [[u8; 75]; 75] {
-    let mut new_world = [[0u8; 75]; 75];
+fn generation(_world: [[u8; 100]; 100]) -> [[u8; 100]; 100] {
+    let mut new_world = [[0u8; 100]; 100];
 
-    for i in 0..74 {
-        for j in 0..74 {
+    for i in 0..99 {
+        for j in 0..99 {
             let mut count = 0;
             if i > 0 {
                 count += _world[i-1][j];
@@ -26,19 +26,19 @@ fn generation(_world: [[u8; 75]; 75]) -> [[u8; 75]; 75] {
             if i > 0 && j > 0 {
                 count += _world[i-1][j+1];
             }
-            if i > 0 && j < 74 {
+            if i > 0 && j < 99 {
                 count += _world[i-1][j+1];
             }
-            if i < 74 {
+            if i < 99 {
                 count += _world[i+1][j];
             }
-            if i < 74 && j < 74 {
+            if i < 99 && j < 99 {
                 count += _world[i+1][j+1];
             }
             if j > 0 {
                 count += _world[i][j-1];
             }
-            if j < 74 {
+            if j < 99 {
                 count += _world[i][j+1];
             }
 
@@ -59,11 +59,11 @@ fn generation(_world: [[u8; 75]; 75]) -> [[u8; 75]; 75] {
 }
 
 fn main() {
-    let mut world = [[0u8; 75]; 75];
+    let mut world = [[0u8; 100]; 100];
     let mut generations = 0;
 
-    for i in 0..74 {
-        for j in 0..74 {
+    for i in 0..99 {
+        for j in 0..99 {
             if rand::random() {
                 world[i][j] = 1;
             } else {
