2feab86ae04ac227ff07a002a20e6313aee7ebab
Haf <haf@protonmail.ch>
2022-12-18 13:27:31 +0100

    ch-3, serde

---
+++ ch-3/ch-3-serde/src/main.rs
@@ -0,0 +1,55 @@
+extern crate serde_derive;
+extern crate serde;
+extern crate serde_json;
+
+use serde::{Deserialize, Serialize};
+use serde_json::Result;
+use std::fs::{File, OpenOptions};
+
+#[derive(Serialize, Deserialize)]
+struct Dvd {
+    name: String,
+    year: u16,
+    cast: String,
+    length: u16,
+}
+
+fn json_from_str(raw: &str) -> Dvd {
+   serde_json::from_str(raw).unwrap()
+}
+
+fn str_from_json(advd: &Dvd) -> String {
+    serde_json::to_string(advd).unwrap()
+}
+
+fn dvds_to_file(f: &String, d: Dvd) {
+    let file = OpenOptions::new().append(true).open(f).unwrap();
+    serde_json::to_writer(file, &d).unwrap();
+}
+
+fn dvds_from_file(f: &String) -> Dvd {
+    let file = File::open(f).unwrap();
+    let deserialized_json: Dvd = serde_json::from_reader(file).unwrap();
+    deserialized_json
+}
+
+fn main() {
+    let raw_data = r#"{
+        "name": "La La Land",
+        "year": 2016,
+        "cast": "Emma Stone, Ryan Gosling",
+        "length": 126
+    }"#;
+
+    let mut d: Dvd = json_from_str(raw_data);
+
+    let encoded = str_from_json(&d);
+
+    println!("{}", encoded);
+
+    let filename = String::from("file.json");
+    dvds_to_file(&filename, d);
+    
+    d = dvds_from_file(&filename);
+    println!("{}", str_from_json(&d));
+}
