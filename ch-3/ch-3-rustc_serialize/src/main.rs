c7775e5ed9bf2fbc5ae2fd46cd9b98c6796e87e3
Haf <haf@protonmail.ch>
2022-12-18 13:14:49 +0100

    ch-3 first version

---
+++ ch-3/ch-3-rustc_serialize/src/main.rs
@@ -0,0 +1,33 @@
+extern crate rustc_serialize;
+use rustc_serialize::json::{self, ToJson, Json};
+
+#[derive(RustcEncodable)]
+struct Dvd {
+    name: String,
+    year: u16,
+    cast: String,
+    length: u16,
+}
+
+impl ToJson for Dvd {
+    fn to_json(&self) -> Json {
+        Json::String(format!("{}+{}+{}+{}i", self.name, self.year, self.cast, self.length))
+    }
+}
+
+fn convert_to_json(advd: &Dvd) -> String {
+    json::encode(advd).unwrap()
+}
+
+fn main() {
+    let a = Dvd {
+        name: String::from("Four Weddings and a Funeral"),
+        year: 1994,
+        cast: String::from("Hugh Grant"),
+        length: 117,
+    };
+
+    let encoded = convert_to_json(&a);
+
+    println!("{}", encoded);
+}
