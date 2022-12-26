4d7272b644c2e8d8e2bc27135b0255e4aa7d2c8d
Haf <haf@protonmail.ch>
2022-12-26 22:39:46 +0100

    correct tokio imports

--- ch-10/src/main.rs
+++ ch-10/src/main.rs
@@ -1,3 +1,14 @@
+use tokio::fs::File;
+use tokio::io::AsyncWriteExt;
+use std::error::Error;
+
+async fn write_to_file(data: &String) -> Result<(), Box<dyn Error>> {
+    let mut output_file = File::create("resp-output.txt").await?;
+    output_file.write_all(data.as_bytes()).await?;
+
+    Ok(())
+}
+
 fn main() {
     println!("Hello, world!");
 }
