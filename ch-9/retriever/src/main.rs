ec270a11ce61f70b1bf648888dd0e65bfa80e0dc
Haf <haf@protonmail.ch>
2022-12-26 16:58:17 +0100

    retriever

--- ch-9/retriever/src/main.rs
+++ ch-9/retriever/src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use mongodb::{
+    bson::{doc, Bson},
+    sync::Client,
+};
+use std::io;
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let client = Client::with_uri_str("mongodb://localhost:27017")?;
+    let collection = client.database("customer_info").collection("people");
+
+    println!("What person would you like to look up?");
+    let mut input = String::new();
+
+    io::stdin().read_line(&mut input)?;
+
+    let results = collection.find(doc! { "name": input.trim() }, None)?;
+
+    for result in results {
+        if let Some(location) = result?.get("location").and_then(Bson::as_str) {
+            println!("location: {}", location);
+        } else {
+            println!("no location listed");
+        }
+    }
+
+    Ok(())
 }
