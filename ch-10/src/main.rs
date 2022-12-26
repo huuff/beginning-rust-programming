1c489d27661709757dcf5471b0450544e74247b3
Haf <haf@protonmail.ch>
2022-12-26 23:05:56 +0100

    ch 10

--- ch-10/src/main.rs
+++ ch-10/src/main.rs
@@ -3,7 +3,8 @@ use tokio::io::AsyncWriteExt;
 use std::error::Error;
 use voca_rs::*;
 use hyper_tls::HttpsConnector;
-use hyper::Client;
+use hyper::{Client, body::HttpBody as _};
+use std::env;
 
 async fn write_to_file(data: &String) -> Result<(), Box<dyn Error>> {
     let mut output_file = File::create("resp-output.txt").await?;
@@ -12,16 +13,49 @@ async fn write_to_file(data: &String) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-fn print_to_scren(data: &String) {
+fn print_to_screen(data: &String) {
     let stripped = strip::strip_tags(data);
     let clean = stripped.replace("\n\n", "");
     println!("{}", clean);
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn Error>> {
     let https = HttpsConnector::new();
     let client = Client::builder().build::<_, hyper::Body>(https);
+    let mut hostname = String::new();
+    let mut write_file = false;
+    let mut print_file = false;
+    let mut body = String::new();
 
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    for position in 1..(args.len()) {
+        if position == args.len()-1 {
+            hostname = String::from(&args[position]);
+        }
+
+        match args[position].as_str() {
+            "-w" => write_file = true,
+            "-p" => print_file = true,
+            _ => (),
+        }
+    }
+
+    let mut res = client.get(hostname.parse()?).await?;
+    println!("Headers:\n{:#?}", res.headers());
+
+    while let Some(chunk) = res.body_mut().data().await {
+        let chunk = chunk?;
+        body.push_str(&(String::from_utf8_lossy(&chunk)));
+    }
+
+    if write_file {
+        write_to_file(&body).await?;
+    }
+
+    if print_file {
+        print_to_screen(&body);
+    }
+
+    Ok(())
 }
