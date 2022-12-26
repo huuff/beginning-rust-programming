91713cb1a7dbf4e00261c62f28e8351965cfe116
Haf <haf@protonmail.ch>
2022-12-26 12:08:22 +0100

    added clap

---
+++ ch-8/src/args.rs
@@ -0,0 +1,16 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+pub struct Args {
+    #[command(subcommand)]
+    pub command: Command,
+}
+
+#[derive(Subcommand, Debug)]
+pub enum Command {
+    #[command(about = "Add record")]
+    Add,
+    #[command(about = "List records")]
+    List,
+}
--- ch-8/src/main.rs
+++ ch-8/src/main.rs
@@ -1,24 +1,19 @@
-use std::env;
 use std::error::Error;
+use clap::Parser;
 use database::Database;
+use args::{Args, Command};
 
 mod database;
+mod args;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let database = Database::new()?;
 
-    let args: Vec<String> = env::args().collect();
+    let args = Args::parse();
 
-    if args.len() > 1 {
-        let command: &str = &args[1];
-
-        match command {
-            "add" => database.add_record()?,
-            "list" => database.list_records()?,
-            _ => println!("Didn't send a valid command in"),
-        }
-    } else {
-        println!("Please specify add or list as a command line parameter");
+    match args.command {
+        Command::Add => database.add_record()?,
+        Command::List => database.list_records()?,
     }
 
     Ok(())
