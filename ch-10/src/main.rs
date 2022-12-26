193019ba0192fed73bdc31092ab301af9c8900ec
Haf <haf@protonmail.ch>
2022-12-26 22:58:49 +0100

    deps for hyper

--- ch-10/src/main.rs
+++ ch-10/src/main.rs
@@ -1,6 +1,9 @@
 use tokio::fs::File;
 use tokio::io::AsyncWriteExt;
 use std::error::Error;
+use voca_rs::*;
+use hyper_tls::HttpsConnector;
+use hyper::Client;
 
 async fn write_to_file(data: &String) -> Result<(), Box<dyn Error>> {
     let mut output_file = File::create("resp-output.txt").await?;
@@ -9,6 +12,16 @@ async fn write_to_file(data: &String) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-fn main() {
+fn print_to_scren(data: &String) {
+    let stripped = strip::strip_tags(data);
+    let clean = stripped.replace("\n\n", "");
+    println!("{}", clean);
+}
+
+#[tokio::main]
+async fn main() {
+    let https = HttpsConnector::new();
+    let client = Client::builder().build::<_, hyper::Body>(https);
+
     println!("Hello, world!");
 }
