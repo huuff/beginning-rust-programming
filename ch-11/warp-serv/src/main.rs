2df2b1dacb1f8bb65da78ea8a470e0b00ad6b353
Haf <haf@protonmail.ch>
2022-12-31 16:41:24 +0100

    warp example

--- ch-11/warp-serv/src/main.rs
+++ ch-11/warp-serv/src/main.rs
@@ -3,5 +3,22 @@ use std::fs;
 
 #[tokio::main]
 async fn main() {
+    let bacon_contents = fs::read_to_string("bacon.txt")
+                            .expect("Unable to open bacon.txt file");
 
+    let bacon = warp::path("bacon").map(move || {
+        format!("{}", &bacon_contents)
+    });
+
+    let hello = warp::path!("hello" / "you").map(|| "Hello, you\n");
+
+    let bye = warp::path("bye")
+                    .and(warp::path::param())
+                    .map(|name: String| format!("Goodbye, {}\n", name));
+
+    let routes = warp::get().and(bacon.or(hello).or(bye));
+
+    warp::serve(routes)
+        .run(([127, 0, 0, 1], 8080))
+        .await;
 }
