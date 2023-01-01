1af4b853675a79c2cdbd9d36f3c5b6958b04d3f8
Haf <haf@protonmail.ch>
2023-01-01 14:07:12 +0100

    did ex1

--- ch-14/temp/src/main.rs
+++ ch-14/temp/src/main.rs
@@ -1,6 +1,10 @@
+mod args;
+
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::error::Error;
+use args::{Args, Command};
+use clap::Parser;
 
 #[derive(Copy, Clone)]
 struct Temperature {
@@ -8,16 +12,21 @@ struct Temperature {
     maximum: f32,
 }
 
-fn get_average(temps: Vec<Temperature>) -> (f32, f32) {
+fn get_totals(temps: &Vec<Temperature>) -> (f32, f32) {
     let mut min_total = 0_f32;
     let mut max_total = 0_f32;
-    let mut count = 0;
 
     for t in temps {
         min_total = min_total + t.minimum;
         max_total = max_total + t.maximum;
-        count += 1;
     }
+
+    (min_total, max_total)
+}
+
+fn get_average(temps: &Vec<Temperature>) -> (f32, f32) {
+    let (min_total, max_total) = get_totals(temps);
+    let count = temps.len();
 
     (min_total / count as f32, max_total / count as f32)
 }
@@ -40,8 +49,19 @@ fn main() -> Result<(), Box<dyn Error>> {
         daily_temps.push(today);
     }
 
-    let avgs = get_average(daily_temps);
-    println!("Average daily low: {}, average daily high: {}", avgs.0, avgs.1);
+    let command = Args::parse().command.unwrap_or(Command::Average);
+
+    match command {
+        Command::Average => {
+            let avgs = get_average(&daily_temps);
+            println!("Average daily low: {}, average daily high: {}", avgs.0, avgs.1);
+        }
+        Command::Total => {
+            let (min_total, max_total) = get_totals(&daily_temps);
+            println!("Total daily low: {min_total}, total daily high: {max_total}");
+        }
+    }
+
 
     Ok(())
 }
