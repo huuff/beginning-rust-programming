a52801c7fe9a4c04dd7fa0baf38aac27dd214bbf
Haf <haf@protonmail.ch>
2023-01-01 11:32:24 +0100

    example from ch-13

--- ch-13/src/main.rs
+++ ch-13/src/main.rs
@@ -1,3 +1,29 @@
+use syslog::{Facility, Formatter3164,};
+use sysinfo::{get_current_pid, PidExt};
+use std::error::Error;
+
+fn get_process_name() -> String {
+    let this_process = std::env::current_exe().unwrap();
+    let this_file = this_process.file_name().unwrap();
+    
+    String::from(this_file.to_str().unwrap())
+}
+
+fn log(message: &str) -> Result<(), Box<dyn Error>> {
+    let this_pid = get_current_pid().unwrap();
+    let formatter = Formatter3164 {
+        facility: Facility::LOG_USER,
+        hostname: None,
+        process: get_process_name(),
+        pid: this_pid.as_u32(),
+    };
+
+    let mut writer = syslog::unix(formatter)?;
+    writer.err(message)?;
+
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    log("This is a log message").unwrap();
 }
